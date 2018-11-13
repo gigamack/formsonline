a97788e4d2df565b9009ad0e2d1b50f52c83c96f
Nitiwat Thongkao <nitiwat007@gmail.com>
2018-11-13 10:22:34 +0700

    Add Student_model

---
+++ application/models/Student_model.php
@@ -0,0 +1,12 @@
+<?php
+
+class Student_model extends CI_Model
+{
+    private $StudentInfo;
+    public function getStudentInfo($StudentID)
+    {
+        $this->load->library('studentinfo');
+        $this->StudentInfo = $this->studentinfo->getStudentInfo($StudentID);
+        return $this->StudentInfol;
+    }
+}

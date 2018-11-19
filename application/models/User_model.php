e6fcafa735fc673ec498a16ed19015f13c205545
Nitiwat Thongkao <nitiwat007@gmail.com>
2018-11-19 09:56:07 +0700

    edit format code

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -7,20 +7,22 @@ class User_model extends CI_Model
     private $Username;
     private $UserType;
     private $StudentInfo;
+    private $AuthenticationResult;
 
     private function setUserInfo()
     {
         $this->load->library('psupassport', $this->credential);
         $this->load->library('roleprovider');
         $this->load->library('studentinfo');
+
         $PSUPassportResult = $this->psupassport->GetUserDetails();
-        $AuthenticationResult = (isset($PSUPassportResult['GetUserDetailsResult']) ? 1 : 0);  // 1 = true 0 = false
-        $this->Username = (($AuthenticationResult==1) ? $PSUPassportResult['GetUserDetailsResult']['string'][0] : '');
+        $this->AuthenticationResult = (isset($PSUPassportResult['GetUserDetailsResult']) ? 1 : 0); // 1 = true 0 = false
+        $this->Username = (($this->AuthenticationResult == 1) ? $PSUPassportResult['GetUserDetailsResult']['string'][0] : '');
         $UserRoles = $this->roleprovider->GetRoles($this->Username);
-        $this->UserType = (($AuthenticationResult==1) ? $this->getUserType($PSUPassportResult['GetUserDetailsResult']['string'][14]) : '');
+        $this->UserType = (($this->AuthenticationResult == 1) ? $this->getUserType($PSUPassportResult['GetUserDetailsResult']['string'][14]) : '');
         $this->StudentInfo = $this->studentinfo->getStudentInfo($this->credential['username']);
         $this->UserInfo = array(
-            "AuthenticationResult" => $AuthenticationResult,
+            "AuthenticationResult" => $this->AuthenticationResult,
             "UserType" => $this->UserType,
             "PSUPassport" => $PSUPassportResult,
             "UserRoles" => $UserRoles,

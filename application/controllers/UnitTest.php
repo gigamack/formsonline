c8f6eab169ab12ab7f0b68d9bf40f76f40949af1
Nitiwat Thongkao <nitiwat007@gmail.com>
2018-11-20 14:13:01 +0700

    Edit UserModel and Add UnitTest

---
+++ application/controllers/UnitTest.php
@@ -0,0 +1,42 @@
+<?php
+
+class UnitTest extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('unit_test');
+        $this->config->item('config');
+
+        $this->load->model('User_model');
+        $this->load->model('docModel');
+
+        
+    }
+
+    public function index()
+    {
+        $credentail = $this->config->item('test_credentail_success');
+        $UserInfo = $this->User_model->getUserInfo($credentail);
+        $AuthenticationResult = $UserInfo['AuthenticationResult'];
+        $test = $AuthenticationResult;
+        $expected_result = 1;
+        $test_name = "Authentication Success";
+        $this->unit->run($test, $expected_result, $test_name);
+
+        // $credentail = $this->config->item('test_credentail_fail');
+        // $UserInfo = $this->User_model->getUserInfo($credentail);
+        // $AuthenticationResult = $UserInfo['AuthenticationResult'];
+        // $test = $AuthenticationResult;
+        // $expected_result = 0;
+        // $test_name = "Authentication Fail";
+        // $this->unit->run($test, $expected_result, $test_name);
+
+
+
+
+
+
+        echo $this->unit->report();
+    }
+}
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -12,11 +12,15 @@ class User_model extends CI_Model
     private function setUserInfo()
     {
         $this->load->library('psupassport', $this->credential);
+        $PSUPassport = new PSUPassport($this->credential);
+        
         $this->load->library('roleprovider');
         $this->load->library('studentinfo');
 
-        $PSUPassportResult = $this->psupassport->GetUserDetails();
+        
+        $PSUPassportResult = $PSUPassport->GetUserDetails();
         $this->AuthenticationResult = (isset($PSUPassportResult['GetUserDetailsResult']) ? 1 : 0); // 1 = true 0 = false
+        
         $this->Username = (($this->AuthenticationResult == 1) ? $PSUPassportResult['GetUserDetailsResult']['string'][0] : '');
         $UserRoles = $this->roleprovider->GetRoles($this->Username);
         $this->UserType = (($this->AuthenticationResult == 1) ? $this->getUserType($PSUPassportResult['GetUserDetailsResult']['string'][14]) : '');
@@ -45,16 +49,3 @@ class User_model extends CI_Model
     }
 
 }
-
-// Array ( [0] => nitiwat.t
-// [1] => นิติวัฒน์
-// [2] => ทองขาว
-// [3] => 0016508
-// [4] => M
-// [5] => 1929900008642
-// [6] => D270
-// [7] => F58
-// [8] => สำนักงานอธิการบดีวิทยาเขตภูเก็ต สำนักงานอธิการบดี วิทยาเขตภูเก็ [9] => C03 [10] => วิทยาเขตภูเก็ต [11] => T01
-// [12] => นาย
-// [13] => nitiwat.t@phuket.psu.ac.th
-// [14] => CN=0016508-nitiwat,OU=D270,OU=F58,OU=C03,OU=Staffs,DC=psu,DC=ac,DC=th )

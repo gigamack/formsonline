b7510032f4d6488262a17b63e07d42c1fce3fac5
Nitiwat Thongkao <nitiwat007@gmail.com>
2019-04-25 15:11:00 +0700

    check UserRoles

--- application/controllers/Authentication.php
+++ application/controllers/Authentication.php
@@ -18,49 +18,34 @@ class Authentication extends CI_Controller
         $UserInfo = $this->User_model->getUserInfo($credentail);
         // print_r($UserInfo);
         $_SESSION['userSession'] = $UserInfo;
-        if($UserInfo['AuthenticationResult']==1)
-        {
-            if ($_SESSION['userSession']['PSUPassport']['GetUserDetailsResult']['string']['9']=='C03')
-            {
-                if ($UserInfo['UserType'] == 'Staffs') {
-                    // redirect(base_url("FormControl/formindexAdmin"));
+        if ($UserInfo['AuthenticationResult'] == 1) {
+            if ($_SESSION['userSession']['PSUPassport']['GetUserDetailsResult']['string']['9'] == 'C03') {
+                 if (empty($UserInfo['UserRoles']['result'])==false) {
                     redirect(base_url('FormControl/stdCardFormAdmin'));
-                    // redirect(base_url("formcontrol/stdCardFormAdmin"));
-                    // echo $_SESSION['userSession']['PSUPassport']['GetUserDetailsResult']['string']['0'];
-                } else 
-                {
-                    // redirect(base_url("FormControl/stdCardMain"));
-                    $_SESSION["ddlchosen"]='0';
+                } else {
+                    $_SESSION["ddlchosen"] = '0';
                     redirect(base_url("FormControl/stdMain"));
-                    // print_r($_SESSION['userSession']['StudentInfo']);
-                    // echo $_SESSION['userSession']['StudentInfo']['STUD_NAME_THAI'].' '.$_SESSION['userSession']['StudentInfo']['STUD_SNAME_THAI'];
                 }
-            }
-            else
-            {
+            } else {
                 $this->logout();
 
             }
-        // print_r($UserInfo['PSUPassport']['GetUserDetailsResult']['string']['9']);
-        }
-        elseif($_POST['password'] = 'test') //addded for test std
+        } elseif ($_POST['password'] = 'test') //addded for test std
         { //addded for test std
             $dataTest = $this->Student_model->getStudentInfo($_POST['username']); //addded for test std
             print_r($dataTest); //addded for test std
             $_SESSION['userSession']['UserType'] = 'Students'; //addded for test std
             $_SESSION['userSession']['StudentInfo']['STUDENT_ID'] = $dataTest['STUDENT_ID']; //addded for test std
-            $_SESSION['userSession']['PSUPassport']['GetUserDetailsResult']['string'][0]= $dataTest['STUDENT_ID']; //addded for test std
-            $_SESSION['userSession']['PSUPassport']['GetUserDetailsResult']['string'][1]= $dataTest['STUD_NAME_THAI']; //addded for test std
-            $_SESSION['userSession']['PSUPassport']['GetUserDetailsResult']['string'][2]= $dataTest['STUD_SNAME_THAI']; //addded for test std
-            $_SESSION['userSession']['StudentInfo']['FAC_NAME_THAI']= $dataTest['FAC_NAME_THAI']; //addded for test std
-            $_SESSION['userSession']['StudentInfo']['MAJOR_NAME_THAI']= $dataTest['MAJOR_NAME_THAI']; //addded for test std
-            $_SESSION["ddlchosen"]='0';
+            $_SESSION['userSession']['PSUPassport']['GetUserDetailsResult']['string'][0] = $dataTest['STUDENT_ID']; //addded for test std
+            $_SESSION['userSession']['PSUPassport']['GetUserDetailsResult']['string'][1] = $dataTest['STUD_NAME_THAI']; //addded for test std
+            $_SESSION['userSession']['PSUPassport']['GetUserDetailsResult']['string'][2] = $dataTest['STUD_SNAME_THAI']; //addded for test std
+            $_SESSION['userSession']['StudentInfo']['FAC_NAME_THAI'] = $dataTest['FAC_NAME_THAI']; //addded for test std
+            $_SESSION['userSession']['StudentInfo']['MAJOR_NAME_THAI'] = $dataTest['MAJOR_NAME_THAI']; //addded for test std
+            $_SESSION["ddlchosen"] = '0';
             redirect(base_url("FormControl/stdMain"));
-            // redirect(base_url("FormControl/stdCardMain")); //addded for test std
         } //addded for test std
-        else
-        {
-            $_SESSION['errors']='Fail';
+        else {
+            $_SESSION['errors'] = 'Fail';
             redirect(base_url());
 
         }
@@ -71,5 +56,5 @@ class Authentication extends CI_Controller
         session_destroy();
         redirect(base_url());
     }
-    
+
 }

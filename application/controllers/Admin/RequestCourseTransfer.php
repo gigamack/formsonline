1e281979bea0f0434b3988d32da8fba9e923b1d6
Nitiwat Thongkao <nitiwat007@gmail.com>
2019-07-19 16:27:35 +0700

    add view action for TransferSubject admin form

--- application/controllers/Admin/RequestCourseTransfer.php
+++ application/controllers/Admin/RequestCourseTransfer.php
@@ -6,11 +6,29 @@ class RequestCourseTransfer extends CI_Controller
         parent::__construct();
         $this->load->model("DocumentModel");
         $this->load->model("DocumentStateModel");
+        $this->load->model('Student_model');
+        $this->load->model('TransferSubjectRequestModel');
+        $this->load->model('TransferSubjectModel');
         $this->load->model("UserModel");
     }
 
     public function index()
     { }
+    public function Get($DocumentID)
+    {
+        $this->data['UserInfo'] = $this->UserModel;
+        $Document = $this->DocumentModel->getWithDocumentType($DocumentID);
+        $LastState = $this->DocumentStateModel->getLastState($DocumentID);
+        $TransferSubject = $this->TransferSubjectModel->Get($DocumentID);
+        $TransferSubjectRequest = $this->TransferSubjectRequestModel->Get($DocumentID);
+        $this->StudentModel->setStudent($this->Student_model->getStudentInfo($Document[0]->StudentID));
+        $this->data['Document'] = (object) array_merge((array) $Document[0], (array) $this->StudentModel,  (array) $LastState[0]);
+        $this->data['TransferSubject'] = $TransferSubject[0];
+        $this->data['TransferSubjectRequest'] = $TransferSubjectRequest;
+        $this->load->view('dashboard/headerAdmin', $this->data);
+        $this->load->view('student/view/header');
+        $this->load->view('student/view/RequestCourseTransfer');
+    }
     public function approve($DocumentID)
     {
         $StatusID = $_POST['btnApprove'];
@@ -27,6 +45,6 @@ class RequestCourseTransfer extends CI_Controller
         $this->DocumentStateModel->Insert($data);
         $this->DocumentModel->Update($DocumentID, array("StatusID" => $StatusID));
 
-        redirect(base_url()."admin/dashboard");
+        redirect(base_url() . "admin/dashboard");
     }
 }
